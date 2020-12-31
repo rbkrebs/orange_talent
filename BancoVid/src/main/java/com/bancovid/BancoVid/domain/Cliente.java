9c67ecd33480bc537c2dfd6ac85be854827a78ba
Romulo Krebs <romulo.krebs@mail.com>
2020-12-31 15:54:11 -0300

    add bean validations em super class e classe cliente e implementa thymeleaf layout

--- BancoVid/src/main/java/com/bancovid/BancoVid/domain/Cliente.java
+++ BancoVid/src/main/java/com/bancovid/BancoVid/domain/Cliente.java
@@ -1,9 +1,20 @@
 package com.bancovid.BancoVid.domain;
 
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+@MappedSuperclass
 public abstract class Cliente extends AbstractEntity<Long> {
 
+    @NotNull(message = "Nome é um campo obrigatório")
+    @Size(min = 3)
     private String nome;
 
+    @NotNull(message = "E-mail é um campo obrigatório")
+    @Email(message = "O valor não condiz com um e-mail válido")
     private String email;
 
 }
--- BancoVid/src/main/java/com/bancovid/BancoVid/domain/ClienteCivil.java
+++ BancoVid/src/main/java/com/bancovid/BancoVid/domain/ClienteCivil.java
@@ -1,11 +1,28 @@
 package com.bancovid.BancoVid.domain;
 
-import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
+
+@Entity
+@Data
+@Table(name = "cliente_civil",
+        uniqueConstraints =
+        @UniqueConstraint(columnNames = "email", name = "cpf_uk"))
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClienteCivil extends Cliente{
+
 
     private LocalDate dataNascimento;
 
+    @Size(max = 11, min = 11, message = "O valor não condiz com um cpf válido")
     private Long cpf;
 }
